6007d15a5e92806584ceeba587b1b7c3a8075e13
Martin Lalev <martin.laleff@gmail.com>
2020-09-19 16:38:34 +0100

    Initial Commit

---
+++ FlooidImages/FlooidImages.h
@@ -0,0 +1,18 @@
+//
+//  FlooidImages.h
+//  FlooidImages
+//
+//  Created by Martin Lalev on 19/09/2020.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FlooidImages.
+FOUNDATION_EXPORT double FlooidImagesVersionNumber;
+
+//! Project version string for FlooidImages.
+FOUNDATION_EXPORT const unsigned char FlooidImagesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FlooidImages/PublicHeader.h>
+
+
